3c89aa4a8d39270c284a3ef1ce86e72888a71767
dpk <d.p.karpov@gmail.com>
2024-04-19 15:43:42 +0400

    k8sfluxrequiredserviceaccountname

--- validate.go
+++ validate.go
@@ -3,60 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
-
-	onelog "github.com/francoispqt/onelog"
-	corev1 "github.com/kubewarden/k8s-objects/api/core/v1"
 	kubewarden "github.com/kubewarden/policy-sdk-go"
 	kubewarden_protocol "github.com/kubewarden/policy-sdk-go/protocol"
 )
 
 func validate(payload []byte) ([]byte, error) {
-	// Create a ValidationRequest instance from the incoming payload
 	validationRequest := kubewarden_protocol.ValidationRequest{}
-	err := json.Unmarshal(payload, &validationRequest)
-	if err != nil {
-		return kubewarden.RejectRequest(
-			kubewarden.Message(err.Error()),
-			kubewarden.Code(400))
+	if err := json.Unmarshal(payload, &validationRequest); err != nil {
+		return kubewarden.RejectRequest(kubewarden.Message(err.Error()), kubewarden.Code(400))
 	}
 
-	// Create a Settings instance from the ValidationRequest object
-	settings, err := NewSettingsFromValidationReq(&validationRequest)
-	if err != nil {
-		return kubewarden.RejectRequest(
-			kubewarden.Message(err.Error()),
-			kubewarden.Code(400))
+	var resource map[string]interface{}
+	if err := json.Unmarshal([]byte(validationRequest.Request.Object), &resource); err != nil {
+		return kubewarden.RejectRequest(kubewarden.Message(fmt.Sprintf("Cannot decode resource: %s", err.Error())), kubewarden.Code(400))
 	}
 
-	// Access the **raw** JSON that describes the object
-	podJSON := validationRequest.Request.Object
-
-	// Try to create a Pod instance using the RAW JSON we got from the
-	// ValidationRequest.
-	pod := &corev1.Pod{}
-	if err := json.Unmarshal([]byte(podJSON), pod); err != nil {
-		return kubewarden.RejectRequest(
-			kubewarden.Message(
-				fmt.Sprintf("Cannot decode Pod object: %s", err.Error())),
-			kubewarden.Code(400))
+	kind := validationRequest.Request.Kind.Kind
+	spec, found := resource["spec"].(map[string]interface{})
+	if !found {
+		return kubewarden.RejectRequest(kubewarden.Message(fmt.Sprintf("%v does not have a spec field", kind)), kubewarden.NoCode)
 	}
 
-	logger.DebugWithFields("validating pod object", func(e onelog.Entry) {
-		e.String("name", pod.Metadata.Name)
-		e.String("namespace", pod.Metadata.Namespace)
-	})
-
-	if settings.IsNameDenied(pod.Metadata.Name) {
-		logger.InfoWithFields("rejecting pod object", func(e onelog.Entry) {
-			e.String("name", pod.Metadata.Name)
-			e.String("denied_names", strings.Join(settings.DeniedNames, ","))
-		})
-
-		return kubewarden.RejectRequest(
-			kubewarden.Message(
-				fmt.Sprintf("The '%s' name is on the deny list", pod.Metadata.Name)),
-			kubewarden.NoCode)
+	serviceAccountName, found := spec["serviceAccountName"].(string)
+	if !found || serviceAccountName == "" {
+		msg := fmt.Sprintf("%v must specify 'spec.serviceAccountName' value", kind)
+		return kubewarden.RejectRequest(kubewarden.Message(msg), kubewarden.NoCode)
 	}
 
 	return kubewarden.AcceptRequest()
